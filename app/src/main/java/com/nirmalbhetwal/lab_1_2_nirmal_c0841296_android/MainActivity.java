24d32994bd9458f6075f4181e347e872939cc5b2
Nirmal Bhetwal <bhetwalnirmal101@gmail.com>
2022-06-02 00:00:29 -0400

    project initialized

---
+++ app/src/main/java/com/nirmalbhetwal/lab_1_2_nirmal_c0841296_android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nirmalbhetwal.lab_1_2_nirmal_c0841296_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
